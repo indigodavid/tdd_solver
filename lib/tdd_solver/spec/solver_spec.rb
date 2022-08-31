024b8208a799d7366d0bfabdcd782e343d8c3bd7
David Vera Castillo <indigodavid@gmail.com>
2022-08-31 16:56:03 -0500

    Add tests for factorial method

--- lib/tdd_solver/spec/solver_spec.rb
+++ lib/tdd_solver/spec/solver_spec.rb
@@ -1,5 +1,25 @@
 require_relative '../solver'
 
 describe Solver do
-  
+  context 'When factorial method is run' do
+    before :each do
+      @solver = Solver.new
+    end
+
+    it 'Factorial method of 0 is equal to 1' do
+      expect(@solver.factorial(0)).to be == 1
+    end
+
+    it 'Factorial method of 1 is equal to 1' do
+      expect(@solver.factorial(1)).to be == 1
+    end
+
+    it 'Factorial method of 4 is equal to 24' do
+      expect(@solver.factorial(4)).to be == 24
+    end
+
+    it 'Factorial method of 5 is equal to 120' do
+      expect(@solver.factorial(5)).to be == 120
+    end
+  end
 end
